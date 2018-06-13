c2629a27caae6535dd044415e4d46896bff7fb52
Елена Яновская <yanovskaya1997@icloud.com>
2018-06-13 17:11:38 +0300

    Initial Commit

---
+++ VariantGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VariantGenerator
+//
+//  Created by Elena Yanovskaya on 13.06.2018.
+//  Copyright © 2018 Elena Yanovskaya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
